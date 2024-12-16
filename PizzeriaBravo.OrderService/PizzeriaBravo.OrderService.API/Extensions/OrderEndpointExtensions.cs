ff53d2db13a789f6c99de3d7d6a38ec40a0ad85f
Albin <albin.abrahamsson@gothiadigital.com>
2024-12-16 10:28:52 +0100

    test

--- PizzeriaBravo.OrderService/PizzeriaBravo.OrderService.API/Extensions/OrderEndpointExtensions.cs
+++ PizzeriaBravo.OrderService/PizzeriaBravo.OrderService.API/Extensions/OrderEndpointExtensions.cs
@@ -74,7 +74,7 @@ public static class OrderEndpointExtension
             return Results.BadRequest(response);
         }
         //return Results.Created($"/api/orders/{response.Data.Id}", response);
-        return Results.Ok();
+        return Results.Ok(response);
     }
 
     private static async Task<IResult> UpdateOrderStatus(IOrderService<Order> repo, Guid id, OrderStatus status)
