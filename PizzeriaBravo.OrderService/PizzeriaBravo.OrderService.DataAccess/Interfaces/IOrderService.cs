4e880a6cc5b5f945307997b4df94ad598a6f1055
Albin <albin.abrahamsson@gothiadigital.com>
2024-12-19 23:18:25 +0100

    Removed unused code

--- PizzeriaBravo.OrderService/PizzeriaBravo.OrderService.DataAccess/Interfaces/IOrderService.cs
+++ PizzeriaBravo.OrderService/PizzeriaBravo.OrderService.DataAccess/Interfaces/IOrderService.cs
@@ -7,8 +7,6 @@ public interface IOrderService<T> where T : class
 {
     Task<Response<IEnumerable<T>>> GetAllOrdersAsync();
     Task<Response<T>> GetOrderByIdAsync(Guid id);
-    Task<Response<IEnumerable<T>>> GetOrdersByCustomerIdAsync(Guid id);
-    Task<Response<IEnumerable<T>>> GetOrdersByOrderStatusAsync(OrderStatus status);
     Task<Response<T>> CreateOrderAsync(T entity);
     Task<Response<T>> UpdateOrderStatusAsync(Guid id, OrderStatus status);
     Task<Response<Guid>> DeleteOrderAsync(Guid id);
